5e4376561f1cd862b36a68449b1fbbd7d977a233
PrincxDevitt <PrincxDevitt@gmail.com>
2024-11-14 19:40:11 -0500

    Se crearon las pages y rutas para la aplicacion de react(aun estan vacias las paginas)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+/*import { useState } from 'react';*/
+/*import { Counter } from './features/counter/Counter';*/
 import './index.css';
+import { makeStyles } from '@mui/styles';
+
+
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Paypal from './Pages/Paypal';
+import Profile from './Pages/Profile';
+
 
 function App() {
+  const user = "Pepe";
+  const classes = useStyle();
   return (
-    <div className="App">
-      <h1>Bienvenidos</h1>
+    <div className={classes.root}>
+      <Router>
+          {
+            !user ? (<Login />) : (
+              <Routes>
+                <Route path='/Profile' element={<Profile />} />
+                <Route path='/Checkout' element={<Paypal />} />
+                <Route path='/' element={<Home />} />
+              </Routes>
+            )
+          }
+      </Router>
     </div>
   );
 }
+
+const useStyle = makeStyles(() => ({
+  root: {
+    /*backgroundColor: "111",*/
+    minHeight: "100vh",
+  },
+}));
 
 export default App;
